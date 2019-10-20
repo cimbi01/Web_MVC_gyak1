a4b79899e870e7d96f4f8b4d0000b65bb04e25b2
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-10-20 20:11:19 +0200

    Kesz

--- gyak1/Program.cs
+++ gyak1/Program.cs
@@ -8,8 +8,22 @@ namespace gyak1
 {
     class Program
     {
+        delegate int Delegalt(int a, int b);
         static void Main(string[] args)
         {
+            Delegalt anon_osszead = delegate (int a, int b) { return a + b; };
+            Delegalt osszead = (a, b) => { return a + b; };
+            Delegalt kivonas = kivon;
+            Console.WriteLine(anon_osszead(1, 2));
+            Console.WriteLine(osszead(1, 2));
+            Console.WriteLine(kivonas(1, 2));
+            Delegalt alapmuveletek = (a, b) => { return a + b; };
+            alapmuveletek += (a, b) => { return a - b; };
+            alapmuveletek += (a, b) => { return a * b; };
+            alapmuveletek += (a, b) => { return a / b; };
+            Console.WriteLine(alapmuveletek(1, 2));
+            Console.ReadKey();
         }
+        static int kivon(int a, int b) { return a - b; }
     }
 }
